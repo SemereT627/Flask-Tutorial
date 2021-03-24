d1b4a78d1589034addfc1edb68e33f24e0fc34f2
Semere Talegngeta <se.semere.talegngeta@gmail.com>
2021-03-25 00:44:49 +0300

    working on sending a reset password page using gmail coordination

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,15 +1,23 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from flask_mail import Mail
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '069002390106b9152c92826087b74e21'
+app.config['SECRET_KEY'] = '04896b5434aae8e6b770457a6f2cf5cc'
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///site.db"
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
+app.config['MAIL_SERVER']='smtp.googlemail.com'
+app.config['MAIL_PORT']=587
+app.config['MAIL_USE_TLS']=True
+app.config['MAIL_USERNAME'] = os.environ.get('EMAIL_USER')
+app.config['MAIL_PASSWORD'] = os.environ.get('EMAIL_PASS')
+mail = Mail(app)
 
 from flaskblog import routes
